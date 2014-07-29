8cfd0b2cb7484bd77596ae2014f46fdd82d4b22c
administrator <administrator@administrators-MacBook-Pro.local>
2014-07-29 18:11:29 -0400

    Week 4 #2 Connectivity Check in Place

--- Connected_App/app/src/main/java/com/fullsail/connected_app/home.java
+++ Connected_App/app/src/main/java/com/fullsail/connected_app/home.java
@@ -1,18 +1,43 @@
 package com.fullsail.connected_app;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class home extends Activity {
 
+    private TextView errorTextView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ConnectivityManager mgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
         setContentView(R.layout.activity_home);
+        errorTextView = (TextView) findViewById(R.id.errorTextview);
+        checkConnectivity(mgr);
+
     }
+
+
+    public void checkConnectivity (ConnectivityManager manager){
+        NetworkInfo netInfo = manager.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnected()){
+            errorTextView.setText("Connection Established");
+        } else {
+            errorTextView.setText("Error: No Connection!  Please check network connection.");
+        }
+
+    }
+
+
 
 
     @Override
